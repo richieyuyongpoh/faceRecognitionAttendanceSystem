6f61b0ca5dfa8752eadc458943c9a8c8de021993
richieyuyongpoh <86595942+richieyuyongpoh@users.noreply.github.com>
2021-07-08 19:03:38 +0800

    Update faceRecognitionAttendanceSystem.py

--- faceRecognitionAttendanceSystem.py
+++ faceRecognitionAttendanceSystem.py
@@ -105,7 +105,7 @@ def initCheckInModule():
             
   
                 
-         if confirmEnter:
+        if confirmEnter:
  
             
             for (name,pwd) in registerUserInfo:
