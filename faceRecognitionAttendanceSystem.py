365e317a1546a2fda0e8a30657fea1fd531f78ef
richieyuyongpoh <86595942+richieyuyongpoh@users.noreply.github.com>
2021-07-08 22:27:32 +0800

    Update faceRecognitionAttendanceSystem.py

--- faceRecognitionAttendanceSystem.py
+++ faceRecognitionAttendanceSystem.py
@@ -123,7 +123,7 @@ def initCheckInModule():
                 face_distances = face_recognition.face_distance(known_face_encodings(), face_encoding)
                 best_match_index = np.argmin(face_distances)
                 if matches[best_match_index]:
-                    name = known_face_names[best_match_index]
+                    name = known_face_names()[best_match_index]
             
             
                 st.write("")
